989b5be6f4e92ce87cb3d422bf6f4ef3d0f65dc0
unknown <alexander@saynisch.de>
2019-05-25 00:24:01 +0200

    python print

--- includes/scripts/json_maker2.py
+++ includes/scripts/json_maker2.py
@@ -6,25 +6,25 @@ def passer(time, number):
     batch = []
     datagroup = ["gps_query", "accgyr_query", "vehicle_query", "battery_query"]
     for file in datagroup:
-        with open(file+".json", "r+") as jsonFile:
+        with open('includes/scripts/'+file+".json", "r+") as jsonFile:
             data = json.load(jsonFile)
 
         data["where"]["TS"][">="]=int(time)#1558536000000000
         data["max-items"] = number
-        with open(file+".json", "w") as jsonFile:
+        with open('includes/scripts/'+file+".json", "w") as jsonFile:
             json.dump(data, jsonFile)
 
         cmd = 'curl -X GET -s -k -H "Content-Type: application/json" --key RESTTEST_key.pem --cert RESTTEST_cert.pem https://ctpwyd.conti.de:443/data?q=$(cat {}.json | base64 -)'.format(file)
-        os.system(cmd+ '> {}_Data.json'.format(file))
+        os.system(cmd+ '> includes/scripts/{}_Data.json'.format(file))
 
-        with open("{}_Data.json".format(file), "r+") as jsonFile2:
+        with open("includes/scripts/{}_Data.json".format(file), "r+") as jsonFile2:
             data2 = json.load(jsonFile2)
 
         batch.append(data2)
-    with open("passer.json", "w") as jsonFile3:
+    with open("includes/scripts/passer.json", "w") as jsonFile3:
             json.dump(batch, jsonFile3)
 
-    with open("passer.json", "r+") as jsonFile4:
+    with open("includes/scripts/passer.json", "r+") as jsonFile4:
             data3 = json.load(jsonFile4)
     print(data3)
    
