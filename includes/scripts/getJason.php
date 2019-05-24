9bf847cc63024f20183d81b513b3372ce743dabf
unknown <alexander@saynisch.de>
2019-05-24 22:47:47 +0200

    debugging

--- includes/scripts/getJason.php
+++ includes/scripts/getJason.php
@@ -2,8 +2,9 @@
 $timestamp = time();
 $timeString = $timestamp . "000000";
 $command = "python includes/scripts/json_maker2.py" . " " . $timeString . " " . "1";
-
+echo $command;
 exec($command, $output, $ret_code);
+echo "Exit Code: " . $ret_code;
 echo implode($output);
 
 ?>
