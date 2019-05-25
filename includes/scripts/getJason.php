84fcb975adb7a488231c15c8ff995b0b2395de6d
unknown <alexander@saynisch.de>
2019-05-25 02:24:29 +0200

    done now php

--- includes/scripts/getJason.php
+++ includes/scripts/getJason.php
@@ -1,7 +1,7 @@
 <?php
 $timestamp = time();
 $timeString = $timestamp . "000000";
-$command = "sudo python3 includes/scripts/json_maker2.py" . " " . $timeString . " " . "1";
+$command = "python3 includes/scripts/json_maker2.py" . " " . $timeString . " " . "1";
 echo $command;
 exec($command, $output, $ret_code);
 echo "Exit Code: " . $ret_code;
