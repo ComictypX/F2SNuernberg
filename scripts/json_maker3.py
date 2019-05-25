8ed9337e79aa82c2b0aba4fa63a059d9ef0780c0
unknown <alexander@saynisch.de>
2019-05-25 13:58:43 +0200

    debug mode

--- scripts/json_maker3.py
+++ scripts/json_maker3.py
@@ -37,7 +37,7 @@ def passer(time, number):
             data2 = json.load(jsonFile2)
         return data2
         batch.append(data2)
-    return data2
+    return data2.replace("'", "\"")
     
 # print (json.dumps(passer(sys.argv[1], sys.argv[2]), ensure_ascii = 'False'))
 print (passer(sys.argv[1], sys.argv[2]))
