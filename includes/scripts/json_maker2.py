046d89333ac9997c18c221e8964f43adb80f765b
unknown <alexander@saynisch.de>
2019-05-25 11:29:43 +0200

    syntax error

--- includes/scripts/json_maker2.py
+++ includes/scripts/json_maker2.py
@@ -36,4 +36,4 @@ def passer(time, number):
     return batch
     
 # print (json.dumps(passer(sys.argv[1], sys.argv[2]), ensure_ascii = 'False'))
-passer(sys.argv[1], sys.argv[2]), ensure_ascii = 'False')
+passer(sys.argv[1], sys.argv[2])
