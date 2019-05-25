fcbafc7da5beb20a9bbf988672d022593dd77213
unknown <alexander@saynisch.de>
2019-05-25 10:18:17 +0200

    scheiß drauf, absolute pfade überall

--- includes/scripts/json_maker2.py
+++ includes/scripts/json_maker2.py
@@ -7,18 +7,18 @@ def passer(time, number):
     batch = []
     datagroup = ["gps_query, accgyr_query", "vehicle_query", "battery_query"]#
     for file in datagroup:
-        with open('includes/scripts/'+file+".json", "r+") as jsonFile:
+        with open('/var/www/html/includes/scripts/'+file+".json", "r+") as jsonFile:
             data = json.load(jsonFile)
 
         data["where"]["TS"][">="]=int(time)#1558536000000000
         data["max-items"] = int(number)
-        with open('includes/scripts/'+file+".json", "w") as jsonFile:
+        with open('/var/www/html/includes/scripts/'+file+".json", "w") as jsonFile:
             json.dump(data, jsonFile, sort_keys=True)
 
-        cmd = 'sudo curl -X GET -s -k -H "Content-Type: application/json" --key includes/scripts/RESTTEST_key.pem --cert includes/scripts/RESTTEST_cert.pem https://ctpwyd.conti.de:443/data?q=$(cat includes/scripts/{}.json | base64 -w 0)'.format(file)
-        os.system(cmd+ '> includes/scripts/{}_Data.json'.format(file))
+        cmd = 'sudo curl -X GET -s -k -H "Content-Type: application/json" --key /var/www/html/includes/scripts/RESTTEST_key.pem --cert /var/www/html/includes/scripts/RESTTEST_cert.pem https://ctpwyd.conti.de:443/data?q=$(cat /var/www/html/includes/scripts/{}.json | base64 -w 0)'.format(file)
+        os.system(cmd+ '> /var/www/html/includes/scripts/{}_Data.json'.format(file))
         
-        with open("includes/scripts/{}_Data.json".format(file), "r+") as jsonFile2:
+        with open("/var/www/html/includes/scripts/{}_Data.json".format(file), "r+") as jsonFile2:
             data2 = json.load(jsonFile2)
 
         batch.append(data2)
