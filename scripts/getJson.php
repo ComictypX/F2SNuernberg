5d92c55c31636013be82d2b3af3df37e0643b7ff
unknown <alexander@saynisch.de>
2019-05-25 13:50:29 +0200

    debug mode

--- scripts/getJson.php
+++ scripts/getJson.php
@@ -4,7 +4,7 @@ $timeString = $timestamp . "000000";
 $command = "python3 /var/www/html/scripts/json_maker3.py" . " " . $timeString . " " . "1 2>&1";
 #$command = "php includes/scripts/test.php 2>&1";
 
-echo $command;
+#echo $command;
 $output = shell_exec($command);
 error_log(print_r($output, TRUE));
 echo $output;
