bef7ad8df78987fcc34592eba113ecee0eef2e24
unknown <alexander@saynisch.de>
2019-05-25 10:13:23 +0200

    fixed filepath again

--- includes/scripts/getJson.php
+++ includes/scripts/getJson.php
@@ -1,7 +1,7 @@
 <?php
 $timestamp = time();
 $timeString = $timestamp . "000000";
-$command = "python3 includes/scripts/json_maker2.py" . " " . $timeString . " " . "1 2>&1";
+$command = "python3 /var/www/html/includes/scripts/json_maker2.py" . " " . $timeString . " " . "1 2>&1";
 #$command = "php includes/scripts/test.php 2>&1";
 
 #echo $command;
