a9457613b59291f7b0484dc8e815f018436152ec
unknown <alexander@saynisch.de>
2019-05-25 09:00:11 +0200

    fixed typo

--- includes/scripts/getJson.php
+++ includes/scripts/getJson.php
@@ -4,6 +4,7 @@ $timeString = $timestamp . "000000";
 $command = "python3 includes/scripts/json_maker2.py" . " " . $timeString . " " . "1";
 #echo $command;
 exec($command, $output);
-#echo "Exit Code: " . $ret_code;
-echo "'" . $output . "'";
+
+#echo $output;
+echo implode("|",$output);
 ?>
