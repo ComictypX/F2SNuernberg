a431df46a9ecbf6ef9c2c5d4ca2d9a66253f65c0
unknown <alexander@saynisch.de>
2019-05-25 11:36:39 +0200

    syntax error

--- includes/scripts/json_maker2.py
+++ includes/scripts/json_maker2.py
@@ -24,7 +24,9 @@ def passer(time, number):
             json.dump(data, jsonFile, sort_keys=True)
         test = json.dumps(data)
         print(test)
-        data = base64.urlsafe_b64encode(test)
+        bencoded = base64.b64encode(test)
+        print(bencoded)
+        data = base64.urlsafe_b64encode(bencoded)
         #os.system('cat /var/www/html/includes/scripts/{}.json | base64 -w 0'.format(file) > 
         #cmd2 = urlencode(cmd2)
         print(data)
